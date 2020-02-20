27185f12be31ab5117d7d8a41334f50e4d8d2d52
durck <poiulkjhmnv50@gmail.com>
2020-02-20 13:22:40 +0300

    test

--- rggu_helper/rggu_helper.user.js
+++ rggu_helper/rggu_helper.user.js
@@ -11,9 +11,6 @@
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @include      https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js
 // @include      https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        none
 // ==/UserScript==
 
 function wrapper(plugin_info) {
